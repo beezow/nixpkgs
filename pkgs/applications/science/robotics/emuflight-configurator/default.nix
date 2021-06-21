6cd40b85f538685917e4ee839738c2110c5ab381
beezow <zbeezow@gmail.com>
2021-06-20 20:35:52 -0600

    missing gulp after ln node_modules in buildPhase

--- pkgs/applications/science/robotics/emuflight-configurator/default.nix
+++ pkgs/applications/science/robotics/emuflight-configurator/default.nix
@@ -1,4 +1,5 @@
-{lib, stdenv, fetchurl, unzip, makeDesktopItem, nwjs, wrapGAppsHook, gsettings-desktop-schemas, gtk3 }:
+{lib, stdenv, callPackage, fetchurl, unzip, makeDesktopItem, nwjs, yarn, nodePackages, yarn2nix, gnutar 
+, libX11, glib, libxcb,  wrapGAppsHook, gsettings-desktop-schemas, gtk3 }:
 
 let
   pname = "emuflight-configurator";
@@ -15,28 +16,44 @@ stdenv.mkDerivation rec {
   inherit pname;
   version = "0.3.5";
   src = fetchurl {
-    url = "https://github.com/emuflight/EmuConfigurator/releases/download/${version}/emuflight-configurator_${version}_linux64.zip";
-    sha256 = "d55bdc52cf93d58c728ccb296ef912a5fc0f42c57ed95f3ded5f85d1c10838c4";
+    url = "https://github.com/emuflight/EmuConfigurator/archive/refs/tags/${version}.tar.gz";
+    sha256 = "9861680694b7bde28fce947159f882fd47dfbc8407a55a647a51e169de0e5b5f";
   };
 
-  nativeBuildInputs = [ wrapGAppsHook unzip ];
 
-  buildInputs = [ gsettings-desktop-schemas gtk3 ];
+  nativeBuildInputs = [ gnutar yarn nwjs wrapGAppsHook nodePackages.gulp nodePackages.node2nix yarn2nix];
 
-  installPhase = ''
-    mkdir -p $out/bin \
-             $out/opt/${pname}
+  buildInputs = [ gsettings-desktop-schemas gtk3 libX11 glib];
 
-    cp -r . $out/opt/${pname}/
-    install -m 444 -D icon/emu_icon_128.png $out/share/icons/hicolor/128x128/apps/${pname}.png
-    cp -r ${desktopItem}/share/applications $out/share/
-
-    makeWrapper ${nwjs}/bin/nw $out/bin/${pname} --add-flags $out/opt/${pname}
+#  configurePhase = ''
+#    node2nix
+#    nix-build default.nix   
+#    mv result
+#  '';
+ # let 
+  nodeDependencies = (callPackage ./node.nix{}).shell.nodeDependencies;
+#  in
+  buildPhase = ''
+    ln -s ${nodeDependencies}/lib/node_modules ./node_modules;
+    export PATH="${nodeDependencies}/bin:$PATH";
+    yarn gulp apps --linux64;
   '';
+
+   installPhase = ''
+      mkdir -p $out/bin \
+              $out/opt/${pname}
+ 
+     cp -r $sourcRoot/apps/${pname}/linux64 $out/opt/${pname}/
+
+     install -m 444 -D icon/emu_icon_128.png $out/share/icons/hicolor/128x128/apps/${pname}.png
+     cp -r ${desktopItem}/share/applications $out/share/
+ 
+     makeWrapper ${nwjs}/bin/nw $out/bin/${pname} --add-flags $out/opt/${pname}
+   '';
 
   meta = with lib; {
     description = "The Emuflight flight control system configuration tool";
-    longDescription = ''
+    longDescription = '' 
       A crossplatform configuration tool for the Emuflight flight control system.
       Various types of aircraft are supported by the tool and by Emuflight, e.g.
       quadcopters, hexacopters, octocopters and fixed-wing aircraft.
